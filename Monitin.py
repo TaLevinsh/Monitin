389ee56ee27015bd15d26e19c2c3ca44efb329b1
TalSaban <Talsabana@gmail.com>
2018-06-27 11:18:30 +0300

    Added Files

--- Monitin.py
+++ Monitin.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 from datetime import *
+import requests
 
 
 def FileToHash(Path):
@@ -17,31 +18,56 @@ def OneFile():
     print 'The File MD5 is:', Hash
 
 
+def Get_Report(Hash):
+    API_Key = '38d010da9b2ebc2def4bb58c1a1f930d22afab1cecf2632d094e52f82be7cc92'
+    Url = 'https://www.virustotal.com/vtapi/v2/file/report'
+    Params = {'apikey': API_Key, 'resource': Hash}
+    Response = requests.get(Url, params=Params)
+    Data = Response.json()
+    return Data
+
+
+def Parse_Report(TheHash):
+    Data = Get_Report(TheHash)
+    Phrase = 'Invalid resource'
+    if Phrase in Data['verbose_msg']:
+        return '0.9'
+    else:
+        X = Data['positives']
+        Y = Data['total']
+        return X/Y
+
+
 def AllFiles():
-    Extensions = ('.txt', '.jpg', '.py', '.pyc', '.h', '.json')
     File_Lst = []
     Exe_Files = []
     DirectoryPath = raw_input('Insert The Directory Path -> ')
-    AllContent = os.listdir(DirectoryPath)
     for Root, Directories, Files in os.walk(DirectoryPath):
         Directories[:] = [D for D in Directories if D not in ['Thumbs']]
         for Filename in Files:
-            if Filename.endswith(Extensions):
-                FilePath = os.path.join(Root, Filename)
-                File_Lst.append(FilePath)
-            if Filename.endswith('.exe'):
+            FilePath = os.path.join(Root, Filename)
+            FPath = open(FilePath, 'rb')
+            File_Content = FPath.read()
+            if File_Content[0:2] == "MZ":
                 FilePath = os.path.join(Root, Filename)
                 Exe_Files.append(FilePath)
+            else:
+                FilePath = os.path.join(Root, Filename)
+                File_Lst.append(FilePath)
+
     DictF = {}
-    datetime.fromtimestamp(1529314845.5859969).strftime('%Y-%m-%d %H:%M:%S')
     for F in File_Lst:
         NameOnlyF = os.path.basename(F)
-        print 'The', NameOnlyF, 'File MD5 Is', FileToHash(F)
-        DictF[NameOnlyF] = FileToHash(F), os.path.getctime(F), 'Not Executable'
+        Date_TimeF = datetime.fromtimestamp(os.path.getctime(F)).strftime('%Y-%m-%d %H:%M:%S')
+        Rate = Parse_Report(FileToHash(F))
+        DictF[NameOnlyF] = FileToHash(F), Date_TimeF, 'Not Executable', Rate
+        print 'The', NameOnlyF, 'File MD5 Is', FileToHash(F), 'Was changed at', Date_TimeF, 'The Rate Is', Rate
     for E in Exe_Files:
         NameOnlyE = os.path.basename(E)
-        print 'The', NameOnlyE, 'Executable File MD5 Is', FileToHash(E)
-        DictF[NameOnlyE] = FileToHash(E), os.path.getctime(E), 'Executable'
+        Date_TimeE = datetime.fromtimestamp(os.path.getctime(E)).strftime('%Y-%m-%d %H:%M:%S')
+        Rate = Parse_Report(FileToHash(E))
+        DictF[NameOnlyE] = FileToHash(E), Date_TimeE, 'Executable', Rate
+        print 'The', NameOnlyE, 'Executable File MD5 Is', FileToHash(E), 'And It Was changed at', Date_TimeE, 'The Rate Is', Rate
     print(DictF)
 
 
